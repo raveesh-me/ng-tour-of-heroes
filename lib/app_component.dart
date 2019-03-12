23035cce46eada912b4f460dcc9eba110df263bc
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-13 05:11:07 +0530

    initial commit

---
+++ lib/app_component.dart
@@ -0,0 +1,19 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'hero.dart';
+
+@Component(
+  selector: 'my-app',
+  template: '''
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}}</h2>
+  <div><label>id:</label>{{hero.id}}</div>
+  <div><label>name:</label>
+  <input [(ngModel)]="hero.name" placeholder="name" ></div>
+  ''',
+  directives: [formDirectives],
+)
+class AppComponent {
+  final title = "Tour Of Heroes";
+  Hero hero = Hero(1, "Windstorm");
+}
