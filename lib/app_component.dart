600d76426c71d948399b83fcded1a31a59180916
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-13 16:51:19 +0530

    on to services

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,10 +2,11 @@ import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:tohx/src/hero.dart';
 import 'package:tohx/src/mock_heroes.dart';
+import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
-  directives: [formDirectives, coreDirectives],
+  directives: [formDirectives, coreDirectives, HeroComponent],
   templateUrl: "app_component.html",
     styleUrls: ["app_component.css"],
 )
