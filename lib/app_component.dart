09d2cd6a1363df79d3e6f0cf3980bf18da885877
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-13 07:45:31 +0530

    refactors html template

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,14 +4,8 @@ import 'hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}}</h2>
-  <div><label>id:</label>{{hero.id}}</div>
-  <div><label>name:</label>
-  <input [(ngModel)]="hero.name" placeholder="name" ></div>
-  ''',
   directives: [formDirectives],
+  templateUrl: "app_component.html"
 )
 class AppComponent {
   final title = "Tour Of Heroes";
