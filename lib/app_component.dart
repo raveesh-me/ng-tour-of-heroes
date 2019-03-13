9bdede79df83f9eecaac945c6db64b5d1f282de7
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-13 13:36:23 +0530

    implementing new component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,17 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-import 'hero.dart';
+import 'package:tohx/src/hero.dart';
+import 'package:tohx/src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
-  directives: [formDirectives],
-  templateUrl: "app_component.html"
+  directives: [formDirectives, coreDirectives],
+  templateUrl: "app_component.html",
+    styleUrls: ["app_component.css"],
 )
 class AppComponent {
   final title = "Tour Of Heroes";
-  Hero hero = Hero(1, "Windstorm");
+  Hero selected;
+  List<Hero> heroes = mockHeroes;
+  onSelect(Hero hero) => selected = hero;
 }
