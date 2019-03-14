ed08d0d1ac2f5a3641f51634826e19e3a660a216
Raveesh Agarwal <raveeshr503@gmail.com>
2019-03-14 06:19:03 +0530

    added an injectable service

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,18 +1,30 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:tohx/src/hero.dart';
-import 'package:tohx/src/mock_heroes.dart';
+import 'package:tohx/src/hero_service.dart';
 import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
   directives: [formDirectives, coreDirectives, HeroComponent],
   templateUrl: "app_component.html",
-    styleUrls: ["app_component.css"],
+  styleUrls: ["app_component.css"],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent extends OnInit {
   final title = "Tour Of Heroes";
+  HeroService _heroService;
+
+  AppComponent(this._heroService);
+
   Hero selected;
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
+  _getHeroes(){
+    heroes = _heroService.getAllHeroes;
+  }
+
   onSelect(Hero hero) => selected = hero;
+
+  @override
+  void ngOnInit() => _getHeroes();
 }
---
+++ lib/src/hero_service.dart
@@ -0,0 +1,6 @@
+import 'package:tohx/src/hero.dart';
+import 'package:tohx/src/mock_heroes.dart';
+
+class HeroService {
+  List<Hero> get getAllHeroes => mockHeroes;
+}
